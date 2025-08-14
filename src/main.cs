054c54e641229a109f9725b2dd783c16c4ce6f42
doxile <feoix75@gmail.com>
2025-08-14 16:01:03 +0700

    Create main.cs

---
+++ src/main.cs
@@ -0,0 +1,13 @@
+using system;
+
+namespace helloworld
+{
+    class program
+    {
+        static void main(string[] args)
+        {
+            console.writeline("hello, world!");
+            // todo
+        }
+    }
+}
